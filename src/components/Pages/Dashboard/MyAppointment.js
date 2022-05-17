53a2d2c082de90df55a3b8d51fd522590f13fe79
Md. Safwan <sayedmdsafwan@gmail.com>
2022-05-17 23:03:25 +0600

    console remove

--- src/components/Pages/Dashboard/MyAppointment.js
+++ src/components/Pages/Dashboard/MyAppointment.js
@@ -23,7 +23,6 @@ const MyAppointment = () => {
                 }
             )
                 .then((res) => {
-                    console.log("res", res);
                     if (res.status === 401 || res.status === 403) {
                         signOut(auth);
                         localStorage.removeItem("accessToken");
